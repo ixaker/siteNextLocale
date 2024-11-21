154a570a1d66d3eb15f69a869fdff93660c28d27
PHlushkov <pavelgluskov264@gmail.com>
2024-11-21 17:22:43 +0200

    fix the bug

--- src/components/header/ui/ThemeToggleButton.tsx
+++ src/components/header/ui/ThemeToggleButton.tsx
@@ -3,20 +3,20 @@ import LightModeIcon from '@mui/icons-material/LightMode';
 import ModeNightIcon from '@mui/icons-material/NightsStay';
 import { useTheme } from '@/context/theme-context/ThemeContext';
 import CustomButton from '@/components/ui/button/CustomButton';
-// import CustomButton from './CustomButton';
-// import { useTheme } from '../context/ThemeContext';
 
 const ThemeToggleButton: React.FC = () => {
   const { themeMode, toggleTheme } = useTheme();
 
   return (
-    <CustomButton
-      variant="button-themes"
-      className="absolute z-20 right-[100px] top-[30px]"
-      onClick={toggleTheme}
-    >
-      {themeMode === 'light' ? <LightModeIcon /> : <ModeNightIcon />}
-    </CustomButton>
+    <>
+      <CustomButton
+        variant="button-themes"
+        className="absolute z-20 right-[100px] top-[30px]"
+        onClick={toggleTheme}
+      >
+        {themeMode === 'light' ? <LightModeIcon /> : <ModeNightIcon />}
+      </CustomButton>
+    </>
   );
 };
 
