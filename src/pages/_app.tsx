076a15cc2dad33a90beabf82101c705bcd2f5f7a
xaker <xaker.dnepr@gmail.com>
2024-11-21 09:45:03 +0200

    adding_theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,57 @@ import '../styles/globals.scss'; // Подключение глобальных 
 import type { AppProps } from 'next/app';
 import { LanguageProvider } from '../context/LanguageContext';
 import Layout from '../components/Layout';
+import { ThemeProvider, CssBaseline, useMediaQuery } from '@mui/material';
+import { useState, useEffect, useMemo } from 'react';
+import { lightTheme, darkTheme } from '../theme'; // Импорт тем
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)'); // Системная тема
+  const [themeMode, setThemeMode] = useState<'light' | 'dark'>('light');
+
+  // Определение начальной темы
+  useEffect(() => {
+    const savedTheme = localStorage.getItem('themeMode');
+    if (savedTheme) {
+      setThemeMode(savedTheme as 'light' | 'dark');
+    } else {
+      setThemeMode(prefersDarkMode ? 'dark' : 'light');
+    }
+  }, [prefersDarkMode]);
+
+  const theme = useMemo(() => (themeMode === 'dark' ? darkTheme : lightTheme), [themeMode]);
+
+  const toggleTheme = () => {
+    const newTheme = themeMode === 'light' ? 'dark' : 'light';
+    setThemeMode(newTheme);
+    localStorage.setItem('themeMode', newTheme);
+  };
+
   return (
-    <LanguageProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </LanguageProvider>
+    <ThemeProvider theme={theme}>
+      <CssBaseline /> {/* Базовые стили MUI */}
+      <LanguageProvider>
+        <Layout>
+          <button
+            onClick={toggleTheme}
+            style={{
+              position: 'fixed',
+              top: '10px',
+              right: '10px',
+              padding: '10px 15px',
+              background: theme.palette.primary.main,
+              color: theme.palette.background.paper,
+              border: 'none',
+              borderRadius: '5px',
+              cursor: 'pointer',
+            }}
+          >
+            Toggle Theme
+          </button>
+          <Component {...pageProps} />
+        </Layout>
+      </LanguageProvider>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme.ts
@@ -0,0 +1,32 @@
+import { createTheme } from '@mui/material/styles';
+
+export const lightTheme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#FFF',
+    },
+    secondary: {
+      main: '#dc004e',
+    },
+  },
+  typography: {
+    fontFamily: 'Roboto, Arial, sans-serif',
+  },
+});
+
+
+export const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#777',
+    },
+    secondary: {
+      main: '#f48fb1',
+    },
+  },
+  typography: {
+    fontFamily: 'Roboto, Arial, sans-serif',
+  },
+});
