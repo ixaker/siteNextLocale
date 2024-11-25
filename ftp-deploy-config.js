d3fc2649cfc790c06149f5f282e960f57949dcb2
xaker <xaker.dnepr@gmail.com>
2024-11-25 09:07:13 +0200

    fix ftp

--- ftp-deploy-config.js
+++ ftp-deploy-config.js
@@ -8,14 +8,38 @@ const config = {
   password: process.env.FTP_PASSWORD,
   host: process.env.FTP_HOST,
   port: 21,
-  localRoot: __dirname + "/out",
-  remoteRoot: "/public_html/",
+  localRoot: "C:/Users/xaker/Downloads/siteNext-main/siteNext-main/out",
+  remoteRoot: "/",
   include: ["*", "**/*"],
   deleteRemote: true,
   forcePasv: true,
 };
 
+// Запуск деплоя
 ftpDeploy
   .deploy(config)
   .then(() => console.log("Деплой завершен!"))
   .catch((err) => console.error("Ошибка деплоя:", err));
+
+// Логирование событий
+
+// Лог каждого загруженного файла
+ftpDeploy.on("uploaded", function (data) {
+  console.log(`Загружен файл: ${data.filename}`);
+});
+
+// Лог прогресса загрузки
+ftpDeploy.on("uploading", function (data) {
+  console.log(`Загрузка: ${data.transferredFileCount}/${data.totalFilesCount} файлов`);
+  console.log(`Текущий файл: ${data.filename}`);
+});
+
+// Лог при удалении файла (если deleteRemote = true)
+ftpDeploy.on("deleted", function (data) {
+  console.log(`Удален файл на сервере: ${data}`);
+});
+
+// Лог завершения деплоя
+ftpDeploy.on("upload-error", function (data) {
+  console.error(`Ошибка загрузки файла ${data.filename}:`, data.err);
+});
