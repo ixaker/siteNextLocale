5924c2082db3f9da77115fd7e77e97f7044bda21
xaker <xaker.dnepr@gmail.com>
2024-11-26 12:29:28 +0200

    fix work menu

--- src/components/header/ui/Menu.tsx
+++ src/components/header/ui/Menu.tsx
@@ -1,48 +1,80 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { Box, Paper } from '@mui/material';
+import React, { useState, useEffect, useRef } from 'react';
+import { Box, Popper, ClickAwayListener, useTheme } from '@mui/material';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import Link from 'next/link';
 import CustomButton from '@/components/ui/button/CustomButton';
 import { PageProps } from '@/context/withStaticPathsAndProps';
 import langUk from '../../../../locales/uk.json';
+import { darkTheme, lightTheme } from '@/theme';
 
 const MenuComponent: React.FC<PageProps> = ({ translations, lang }) => {
-  const [openSubMenu, setOpenSubMenu] = useState<null | string>(null);
-  const menuRef = useRef<HTMLDivElement | null>(null);
-  const closeTimeoutRef = useRef<NodeJS.Timeout | null>(null);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [currentSubMenu, setCurrentSubMenu] = useState<null | string>(null);
+  const theme = useTheme();
+  const observerRef = useRef<IntersectionObserver | null>(null);
+  const hoverTimeoutRef = useRef<NodeJS.Timeout | null>(null);
 
   const menuItems = translations?.menu || langUk.menu;
 
-  const handleSubMenuOpen = (menuTitle: string | null) => {
-    if (closeTimeoutRef.current) {
-      clearTimeout(closeTimeoutRef.current);
+  const handleMenuOpen = (event: React.MouseEvent<HTMLElement>, menuTitle: string) => {
+    setAnchorEl(event.currentTarget);
+    setCurrentSubMenu(menuTitle);
+  };
+
+  const handleMenuClose = () => {
+    if (hoverTimeoutRef.current) {
+      clearTimeout(hoverTimeoutRef.current);
     }
-    setOpenSubMenu(menuTitle);
+    setAnchorEl(null);
+    setCurrentSubMenu(null);
   };
 
-  // Задержка в 200 мс перед закрытием
-  const handleSubMenuClose = () => {
-    closeTimeoutRef.current = setTimeout(() => {
-      setOpenSubMenu(null);
-    }, 200);
+  const handleMouseEnter = (event: React.MouseEvent<HTMLElement>, menuTitle: string) => {
+    if (hoverTimeoutRef.current) {
+      clearTimeout(hoverTimeoutRef.current);
+    }
+    setAnchorEl(event.currentTarget);
+    setCurrentSubMenu(menuTitle);
   };
 
-  // Закрытие меню при клике вне
+  const handleMouseLeave = () => {
+    hoverTimeoutRef.current = setTimeout(() => {
+      setAnchorEl(null);
+      setCurrentSubMenu(null);
+    }, 200); // Добавляем небольшую задержку перед закрытием
+  };
+
+  // Проверка видимости элемента с использованием IntersectionObserver
   useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
-        setOpenSubMenu(null);
+    if (anchorEl) {
+      observerRef.current = new IntersectionObserver(
+        ([entry]) => {
+          if (!entry.isIntersecting) {
+            handleMenuClose();
+          }
+        },
+        {
+          root: null, // Следим за видимостью в пределах viewport
+          threshold: 0.1, // Считаем невидимым, если менее 10% элемента видно
+        }
+      );
+
+      observerRef.current.observe(anchorEl);
+    }
+
+    return () => {
+      if (observerRef.current && anchorEl) {
+        observerRef.current.unobserve(anchorEl); // Убираем наблюдение при размонтировании
+        observerRef.current.disconnect();
       }
     };
+  }, [anchorEl]);
 
-    document.addEventListener('mousedown', handleClickOutside);
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, []);
+  const currentTheme = theme.palette.mode === 'dark' ? darkTheme : lightTheme;
+  const primaryColor = currentTheme.palette.primary.main;
 
   return (
-    <nav className="flex items-center gap-5 sm:gap-[30px] md:gap-[40px] lg:gap-[80px]" ref={menuRef}>
+    <nav className="flex items-center gap-5 sm:gap-[30px] md:gap-[40px] lg:gap-[80px]">
       {menuItems.map((item) => (
         <Box
           key={item.title}
@@ -50,64 +82,84 @@ const MenuComponent: React.FC<PageProps> = ({ translations, lang }) => {
             position: 'relative',
             display: 'flex',
             alignItems: 'center',
-            flexWrap: 'wrap',
           }}
-          onMouseEnter={() => handleSubMenuOpen(item.title)}
-          onMouseLeave={handleSubMenuClose}
+          onMouseEnter={(event) => handleMouseEnter(event, item.title)}
+          onMouseLeave={handleMouseLeave}
         >
           {item.subMenu ? (
             <>
-              <CustomButton ariaLabel={item.title} variant="menu-btn">
+              <CustomButton
+                ariaLabel={item.title}
+                variant="menu-btn"
+                onClick={(event) => handleMenuOpen(event, item.title)}
+              >
                 {item.title}
                 <KeyboardArrowDownIcon
                   style={{
                     transition: 'transform 0.3s ease',
-                    transform: openSubMenu === item.title ? 'rotate(180deg)' : 'rotate(0deg)',
+                    transform: currentSubMenu === item.title ? 'rotate(180deg)' : 'rotate(0deg)',
                   }}
                 />
               </CustomButton>
 
               {/* Всплывающее меню */}
-              {openSubMenu === item.title && (
-                <Paper
-                  elevation={3}
-                  sx={{
-                    position: 'absolute',
-                    top: '100%',
-                    left: 0,
-                    mt: 1,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    zIndex: 1300,
-                    whiteSpace: {
-                      xs: 'wrap',
-                      sm: 'nowrap',
-                      md: 'nowrap',
-                      lg: 'nowrpa',
-                      xl: 'nowrap',
+              <Popper
+                open={currentSubMenu === item.title}
+                anchorEl={anchorEl}
+                placement="bottom-start"
+                modifiers={[
+                  {
+                    name: 'offset',
+                    options: {
+                      offset: [10, 10], // Отступы: 10px по горизонтали, 10px по вертикали
                     },
-                    width: {
-                      xs: 180,
-                      sm: 'auto',
-                      md: 'auto',
-                      lg: 'auto',
-                      xl: 'auto',
+                  },
+                  {
+                    name: 'preventOverflow',
+                    options: {
+                      boundary: 'viewport',
+                      padding: 16, // Минимальный отступ от краёв экрана
                     },
-                  }}
-                  onMouseEnter={() => handleSubMenuOpen(item.title)}
-                  onMouseLeave={handleSubMenuClose}
-                >
-                  {item.subMenu.map((subItem) => (
-                    <Link
-                      key={subItem.title}
-                      className="hover:bg-[#c43c1e] no-wrap transition-all duration-300 ease-in-out p-1 sm:p-1 md:p-1.5 lg:p-2 rounded-[3px] text-[13px] sm:text-[14px] md:text-[15px] lg:text-[17px]"
-                      href={`/${lang}/${subItem.href}/`}
-                    >
-                      {subItem.title}
-                    </Link>
-                  ))}
-                </Paper>
-              )}
+                  },
+                  {
+                    name: 'flip',
+                    options: {
+                      fallbackPlacements: ['bottom-start', 'top-start'], // Попытка расположить меню сверху или снизу
+                    },
+                  },
+                ]}
+                sx={{
+                  zIndex: 1300,
+                  mt: 1,
+                  display: 'flex',
+                  flexDirection: 'column',
+                  backgroundColor: primaryColor,
+                  borderRadius: '4px',
+                  boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
+                  maxHeight: '300px',
+                  overflowY: 'auto',
+                  maxWidth: '90vw',
+                }}
+              >
+                <ClickAwayListener onClickAway={handleMenuClose}>
+                  <Box>
+                    {item.subMenu.map((subItem) => (
+                      <Link
+                        key={subItem.title}
+                        href={`/${lang}/${subItem.href}/`}
+                        className="hover:bg-[#c43c1e] text-[white] transition-all duration-300 ease-in-out p-1 sm:p-1 md:p-1.5 lg:p-2 rounded-[3px] text-[13px] sm:text-[14px] md:text-[15px] lg:text-[17px]"
+                        style={{
+                          textDecoration: 'none',
+                          display: 'block',
+                        }}
+                        onClick={handleMenuClose}
+                      >
+                        {subItem.title}
+                      </Link>
+                    ))}
+                  </Box>
+                </ClickAwayListener>
+              </Popper>
             </>
           ) : (
             <Link href={`/${lang}/${item.href}`} style={{ textDecoration: 'none' }}>
