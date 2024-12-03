0fff52fbd82fa11635aceb594bce57802d31bfff
PHlushkov <pavelgluskov264@gmail.com>
2024-12-03 09:28:06 +0200

    setings file list

--- src/components/ui/modal-window/ui/ListFiles.tsx
+++ src/components/ui/modal-window/ui/ListFiles.tsx
@@ -1,8 +1,8 @@
 import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile';
 import { Typography } from '@mui/material';
 import CustomButton from '../../button/CustomButton';
-import ClearIcon from '@mui/icons-material/Clear';
 import { handleFileRemove } from '../utils/fileHandlers';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 interface ListFilesProps {
   index: number;
@@ -17,22 +17,24 @@ const ListFiles: React.FC<ListFilesProps> = ({ index, fileName, fileSize, setFil
       throw new Error('Input must be a non-negative number');
     }
     const megabytes = Math.round((bytes / (1024 * 1024)) * 100) / 100;
-    return parseFloat(megabytes.toPrecision(3));
+    return parseFloat(megabytes.toPrecision(3)).toFixed(2);
   }
 
   return (
     <li key={index} className="flex justify-between mt-1  items-center">
       <div className="flex items-center gap-2 w-full justify-between">
         <InsertDriveFileIcon />
-        <Typography className="text-nowrap overflow-hidden w-1/2 text-ellipsis " variant="body2">
-          {fileName}
-        </Typography>
-        <Typography variant="body2">{bytesToMegabytes(fileSize)} Mb</Typography>
+        <div className="flex justify-between w-full">
+          <Typography className="text-nowrap overflow-hidden w-1/2 text-ellipsis " variant="body2">
+            {fileName}
+          </Typography>
+          <Typography variant="body2">{bytesToMegabytes(fileSize)} Mb</Typography>
+        </div>
         <CustomButton
-          className="border p-1 text-sm hover:border-red-500 hover:text-red-500 transition-all duration-300 ease-in-out"
+          className="p-1 text-sm hover:border-red-500 hover:text-red-500 transition-all duration-300 ease-in-out"
           onClick={() => handleFileRemove(index, setFileList)}
         >
-          <ClearIcon />
+          <DeleteIcon />
         </CustomButton>
       </div>
     </li>
