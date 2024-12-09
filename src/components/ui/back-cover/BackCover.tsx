5f21e17b2f5fab7714e095a13f7a0315c4eab9de
PHlushkov <pavelgluskov264@gmail.com>
2024-12-09 21:59:54 +0300

    fix background flickering

--- src/components/ui/back-cover/BackCover.tsx
+++ src/components/ui/back-cover/BackCover.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useMediaQuery } from 'react-responsive';
 
 interface BackCoverProps {
@@ -8,14 +8,8 @@ interface BackCoverProps {
 
 const BackCover: React.FC<BackCoverProps> = ({ children, bgImg }) => {
   const isSmallScreen = useMediaQuery({ query: '(max-width: 640px)' });
-  const [backgroundImage, setBackgroundImage] = useState<string | undefined>(bgImg);
-
-  useEffect(() => {
-    if (isSmallScreen) {
-      setBackgroundImage(bgImg || `/bgImg.webp?v=${new Date().getTime()}`);
-    } else {
-      setBackgroundImage(`/bgImg.webp?v=${new Date().getTime()}`);
-    }
+  const backgroundImage = React.useMemo(() => {
+    return isSmallScreen && bgImg ? bgImg : '/bgImg.webp';
   }, [isSmallScreen, bgImg]);
 
   return (
@@ -28,7 +22,6 @@ const BackCover: React.FC<BackCoverProps> = ({ children, bgImg }) => {
       className="relative min-h-[60vh] md:min-h-[70vh] lg:min-h-[80vh] xl:min-h-[90vh] shadow-[0_10px_30px_rgba(0,_0,_0,_0.4)]"
     >
       <div className="absolute inset-0 bg-black bg-opacity-50 z-0 sm:bg-black sm:bg-opacity-0"></div>
-
       {children}
     </div>
   );
